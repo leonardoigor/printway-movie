b010229699f803604ec0cce70cd08987c6c02aff
igor leonardo mendonça <igormendonca@rebortec.com>
2022-11-02 11:21:48 -0300

    entityBase create

---
+++ Movie.Domain/Entities/Base/EntityBase.cs
@@ -0,0 +1,11 @@
+﻿namespace Movie.Domain.Entities.Base;
+using prmToolkit.NotificationPattern;
+
+public class EntityBase:Notifiable
+{
+    protected EntityBase()
+    {
+        Id = Guid.NewGuid();
+    }
+    public Guid Id { get; set; }
+}
