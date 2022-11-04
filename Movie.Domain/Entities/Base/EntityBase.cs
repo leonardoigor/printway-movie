0dd2210b1fb1d81b9262a802793166700d0cca2c
igor leonardo mendonça <igormendonca@rebortec.com>
2022-11-04 12:51:14 -0300

    IRepository base interface

--- Movie.Domain/Entities/Base/EntityBase.cs
+++ Movie.Domain/Entities/Base/EntityBase.cs
@@ -1,11 +1,17 @@
-﻿namespace Movie.Domain.Entities.Base;
-using prmToolkit.NotificationPattern;
+﻿using prmToolkit.NotificationPattern;
 
-public class EntityBase:Notifiable
+namespace Movie.Domain.Entities.Base;
+
+public class EntityBase : Notifiable
 {
     protected EntityBase()
     {
         Id = Guid.NewGuid();
     }
+
     public Guid Id { get; set; }
+
+    protected virtual void NotificationsConfig()
+    {
+    }
 }
