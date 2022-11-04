eb73f75b3f5449bebb824d45dfefbea98151b8fa
igor leonardo mendonça <igormendonca@rebortec.com>
2022-11-04 12:53:46 -0300

    IMovieRepository

---
+++ Movie.Domain/Interfaces/Repositories/IMovieRepository.cs
@@ -0,0 +1,7 @@
+﻿using Movie.Domain.Interfaces.Repositories.Base;
+
+namespace Movie.Domain.Interfaces.Repositories;
+
+public interface IMovieRepository : IRepositoryBase<Entities.Movie, Guid>
+{
+}
