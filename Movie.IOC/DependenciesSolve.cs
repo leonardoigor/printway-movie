6d8c6955f08acb8c1da4f9853af3a2fc0a4a5771
igor leonardo mendonça <igormendonca@rebortec.com>
2022-11-05 14:04:48 -0300

    ioc project

---
+++ Movie.IOC/DependenciesSolve.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Movie.Domain.Interfaces.Repositories.Base;
+using Movie.Infra.Persistence.Contexts;
+
+namespace Movie.IOC;
+
+public static class DependenciesSolve
+{
+    public static IServiceCollection SolveDepencies(this IServiceCollection ser)
+    {
+
+        ser.AddScoped<PopCornContext>();
+        ser.AddTransient<IUnitOfWork, UnitOfWork>();
+
+        // add repositories
+        ser.AddTransient(typeof(IRepositoryBase<,>), typeof(RepositoryBase<,>));
+
+        // ser.AddTransient<IPostRepository, PostRepository>();
+
+        //add services
+        // ser.AddTransient<IPostService, PostService>();
+        //ser.AddTransient<IReactionService, ReactionService>();
+
+        return ser;
+    }
+}
