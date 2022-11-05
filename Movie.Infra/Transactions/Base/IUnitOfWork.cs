73485f8d43f7768ee67fec9114c04e0d700daca3
igor leonardo mendonça <igormendonca@rebortec.com>
2022-11-05 14:06:13 -0300

    transactions with unitWork

---
+++ Movie.Infra/Transactions/Base/IUnitOfWork.cs
@@ -0,0 +1,6 @@
+﻿namespace Movie.Infra.Transactions.Base;
+
+public interface IUnitOfWork
+{
+    void Commit();
+}
---
+++ Movie.Infra/Transactions/UnitOfWork.cs
@@ -0,0 +1,18 @@
+﻿using Movie.Infra.Persistence.Contexts;
+
+namespace Movie.Infra.Transactions;
+
+public class UnitOfWork
+{
+    private readonly PopCornContext _context;
+
+    public UnitOfWork(PopCornContext context)
+    {
+        _context = context;
+    }
+
+    public void Commit()
+    {
+        _context.SaveChanges();
+    }
+}
