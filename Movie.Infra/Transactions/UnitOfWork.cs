dce20e20ebcd25271cf12226383318a8c27b2a15
igor leonardo mendonça <igormendonca@rebortec.com>
2022-11-05 14:06:35 -0300

    transactions with unitWork

--- Movie.Infra/Transactions/UnitOfWork.cs
+++ Movie.Infra/Transactions/UnitOfWork.cs
@@ -1,8 +1,10 @@
 ﻿using Movie.Infra.Persistence.Contexts;
+using Movie.Infra.Transactions.Base;
 
 namespace Movie.Infra.Transactions;
 
-public class UnitOfWork
+public class UnitOfWork : IUnitOfWork
+
 {
     private readonly PopCornContext _context;
 
